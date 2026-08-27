63dc9cc2f6312bdb3c5540ee5801ac41457094f0
Banzinda09 <dornelasricardo86@gmail.com>
2026-08-27 17:45:02 -0300

    colocando a ficha.py

---
+++ ficha.py
@@ -0,0 +1,16 @@
+nome = str(input('qual e o seu nome: '))
+idade = int(input('qual e sua idade: '))
+cidade = str(input('qual e sua cidade: '))
+py = input('voce gosta de programar: ')
+python = py.upper()
+if python == 'SIM':
+    like = True
+else:
+    like = False
+
+print('===== CADASTRO ===== ')
+print(f'Nome: {nome}')
+print(f'Idade: {idade} anos')
+print(f'Cidade: {cidade}')
+print(f'Gosta de programar: {like}')
+print('=====================')
